8e63752882d600a692f055f8398589ae305f28dd
Shubham Singhal <sbmsgl1129@gmail.com>
2019-02-25 11:06:21 +0530

    remove commented lines

--- binary_search/square_troot.py
+++ binary_search/square_troot.py
@@ -5,7 +5,6 @@ class Solution:
 
         start = 0
         end   = A
-        #last_square = -1 # enum would be better choice
 
         while start <= end:
             mid = (start + end) // 2
@@ -16,9 +15,7 @@ class Solution:
             
             if square_val > A:
                 end = mid - 1
-                #last_square = 1
             elif square_val < A:
                 start = mid + 1
-                #last_square = 0
 
         return end
